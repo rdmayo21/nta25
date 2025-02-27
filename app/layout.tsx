347ad290687bf545ea3a75d7c67b1cd03872d556
Daniel Mayo <danielmayo@Daniels-MacBook-Air.local>
2025-02-27 08:44:28 -0600

    Renamed project from McKays app template to voice journal

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Mckay's App Template",
+  title: "Voice Journal",
   description: "A full-stack web app template."
 }
 
